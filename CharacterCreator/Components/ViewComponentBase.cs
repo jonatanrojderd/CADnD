cdd3460714a8f8af21e748f8a2c1e61f93f7fa2a
Jonatan Röjder Delnavaz <jonatanrojderd@gmail.com>
2024-06-22 11:38:27 +0200

    * Created a simple structure to use MVVM together with Blazor

---
+++ CharacterCreator/Components/ViewComponentBase.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Components;
+
+namespace CharacterCreator.Components;
+
+public class ViewComponentBase<TViewModel> : ComponentBase where TViewModel : IViewModel
+{
+    [Inject]
+    [NotNull]
+    protected TViewModel ViewModel { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        ViewModel.PropertyChanged += OnPropertyChanged;
+        await base.OnInitializedAsync();
+    }
+
+    private void OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        StateHasChanged();
+    }
+}
---
+++ CharacterCreator/Components/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CharacterCreator.Components;
+
+public interface IViewModel : INotifyPropertyChanged
+{
+    Task InitializeAsync();
+}
+
+public abstract partial class ViewModelBase : ObservableObject, IViewModel
+{
+    public virtual async Task InitializeAsync()
+    {
+    }
+}
