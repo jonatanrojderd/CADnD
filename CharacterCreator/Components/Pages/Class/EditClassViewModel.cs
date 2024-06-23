547d7c95e9326d7621dcb140b14437cd503cd35e
Jonatan Röjder Delnavaz <jonatanrojderd@gmail.com>
2024-06-24 00:53:55 +0200

    * Implemented the Edit Class logic and view

---
+++ CharacterCreator/Components/Pages/Class/EditClassViewModel.cs
@@ -0,0 +1,50 @@
+﻿using CharacterCreator.Models;
+using CharacterCreator.Utilities;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.AspNetCore.Components;
+
+namespace CharacterCreator.Components.Pages.Class;
+
+public partial class EditClassViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private ClassModel _class = new();
+
+    [ObservableProperty]
+    private IList<AbilityScore> _abilityScores = [];
+    
+    public int ClassIndex { get; set; }
+
+    public override async Task InitializeAsync(IDataSerializer dataSerializer, NavigationManager navigationManager)
+    {
+        await base.InitializeAsync(dataSerializer, navigationManager);
+
+        Class = (await dataSerializer.DeserializeAsync()).Classes[ClassIndex];
+        AbilityScores = Enum.GetValues<AbilityScore>();
+    }
+
+    [RelayCommand]
+    private void AbilityScoreChanged(ChangeEventArgs args)
+    {
+        var abilityScore = Enum.Parse<AbilityScore>(args.Value.ToString());
+        Class.PrimeStat = abilityScore;
+    }
+
+    [RelayCommand]
+    private void HitDieChanged(ChangeEventArgs args)
+    {
+        var hitDie = int.Parse(args.Value.ToString());
+        Class.HitDie = hitDie;
+    }
+
+    [RelayCommand]
+    private async Task Save()
+    {
+        var dataContainer = await DataSerializer.DeserializeAsync();
+        dataContainer.Classes[ClassIndex] = Class;
+
+        await DataSerializer.SerializeAsync(dataContainer);
+        NavigationManager.NavigateTo("/");
+    }
+}
---
+++ CharacterCreator/Components/Pages/Class/EditView.cs
@@ -0,0 +1,22 @@
+﻿namespace CharacterCreator.Components.Pages.Class;
+
+public partial class EditView : ViewComponentBase<EditClassViewModel>
+{
+    protected override async Task OnInitializedAsync()
+    {
+        var parameters = GetQueryParameters(NavigationManager.Uri);
+
+        var classIndex = parameters[0].Split("=")[^1];
+        ViewModel.ClassIndex = int.Parse(classIndex);
+
+        await base.OnInitializedAsync();
+    }
+
+    private static string[] GetQueryParameters(string uri)
+    {
+        var querySplit = uri.Split("?", StringSplitOptions.RemoveEmptyEntries);
+
+        var parameters = querySplit[^1].Split("&");
+        return parameters;
+    }
+}
